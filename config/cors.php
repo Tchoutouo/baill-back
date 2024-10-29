a497a7e3732c8d5315011627217b79ceffff569e
Cedric Asofack <casofackzeu@utecq.dev>
2024-10-29 17:11:14 +0100

    mise à jour

--- config/cors.php
+++ config/cors.php
@@ -20,6 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:4200')],
+    // 'allowed_origins' => [env('FRONTEND_URL', 'https://bailleurnet.com')],
 
     'allowed_origins_patterns' => [],
 
