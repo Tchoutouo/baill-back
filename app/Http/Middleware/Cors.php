ccfc9a992e127b0931aeb4d84ba6fd2d02724665
Yvan Tchoutouo <yvalttnomdecode@gmail.com>
2025-02-04 13:52:34 +0100

    envoi update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -27,7 +27,8 @@ class Cors
         return $next($request)
                 ->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Methods', '*')
-                ->header('Access-Control-Allow-Headers', '*');
+                ->header('Access-Control-Allow-Headers', '*')
+                ->header('Access-Control-Allow-Credentials', 'true');
 
     }
 }
